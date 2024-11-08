6bce79df4514a2f41b2b46034be7a2e08d614693
Javier Parales <alejito09parales@gmail.com>
2024-11-08 15:45:39 -0500

    commit 3

--- src/iug/Welcome.java
+++ src/iug/Welcome.java
@@ -5,10 +5,17 @@ import javax.swing.*;
 public class Welcome {
     // creacion de componentes
     private JPanel Bienvenido;
+    private JLabel saludo;
+
     // ejecuta init
     public Welcome(){
         init();
     }
+
+    public void setNombre(String nombre){
+        saludo.setText("Bienvenido "+nombre);
+    }
+
     // configuracion de los componentes
     private void init(){
         // configuracion de la vista
